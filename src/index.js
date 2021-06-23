06b673453222e9ffac0103c137ab7326d97c277c
Kasia Gierat <kasia.gierat@gmail.com>
2021-06-23 11:51:52 -0400

    readme

--- src/index.js
+++ src/index.js
@@ -3,6 +3,10 @@ import ReactDOM from "react-dom";
 import PinsScreen from "./PinsScreen";
 import jsonData from "./data/nyc_ttp_pins.json";
 
+/**
+ * 
+ * @returns 
+ */
 const App = () => {
   const data = JSON.parse(JSON.stringify(jsonData));
   return (
